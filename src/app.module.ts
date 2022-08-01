1186c463a95c3f6746c1d33bf5a27d8f77fdb5bb
Khanh Nguyen <khanh.nguyen@techupcorp.com>
2022-08-01 23:10:57 +0700

    feat: complete log feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerModule } from 'nestjs-pino';
 import envValidation from './core/env.validation';
 import { RoleGuard } from './core/role.guard';
 import { AuthModule } from './modules/auth/auth.module';
 import { ImageModule } from './modules/image/image.module';
+import { TokenModule } from './modules/token/token.module';
 import { UserController } from './modules/user/user.controller';
 import { UserModule } from './modules/user/user.module';
-import { TokenModule } from './modules/token/token.module';
 
 @Module({
   imports: [
@@ -26,6 +27,31 @@ import { TokenModule } from './modules/token/token.module';
         useUnifiedTopology: true,
       }),
       inject: [ConfigService],
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          targets: [
+            {
+              level: 'info',
+              target: 'pino-pretty',
+              options: {
+                translateTime: true,
+                singleLine: true,
+              },
+            },
+            {
+              level: 'trace',
+              target: 'pino-pretty',
+              options: {
+                translateTime: true,
+                colorize: false,
+                destination: './logs/http.log',
+              },
+            },
+          ],
+        },
+      },
     }),
     UserModule,
     AuthModule,
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './core/response.interceptor';
 import { ValidationPipe } from './core/validation.pipe';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const configService = app.get(ConfigService);
 
   const PORT = configService.get('PORT');
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+  app.useLogger(app.get(Logger));
 
   app.enableCors({
     origin: '*',
